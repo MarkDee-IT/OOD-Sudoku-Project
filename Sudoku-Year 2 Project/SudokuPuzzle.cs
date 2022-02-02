e5ee4f752d6024743c76f862a6e6576e973f6f50
Mark Dee <S00213766@mail.itsligo.ie>
2022-02-02 12:45:15 +0000

    Add project files.

---
+++ Sudoku-Year 2 Project/SudokuPuzzle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku_Year_2_Project
+{
+    class SudokuPuzzle
+    {
+        #region PROPERTIES
+        int[,] _puzzleStructure = new int[9, 9];
+        public int[,] PuzzleStructure 
+        { 
+            get { return _puzzleStructure; }
+            set { _puzzleStructure = value; }
+        }
+
+        public bool Solvable { get; set;}
+        #endregion
+
+        #region CONSTRUCTORS
+        // Blank constructor
+        public SudokuPuzzle()
+        {
+
+        }
+
+        public SudokuPuzzle(int[,] structure)
+        {
+            _puzzleStructure = structure;
+        }
+        #endregion
+    }
+}
